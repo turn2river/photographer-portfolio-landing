007c80d28ba59d160009e9ec28c90e403095e24b
alexey.krylov <turn2river@gmail.com>
2022-01-24 19:07:29 +0300

    feat: continue work with burger-menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,13 +1,26 @@
-console.log('Привет!\n Если у тебя монитор 1440px и меньше, уменьши, пожалуйста, масштаб при проверке :)\n Хорошего дня тебе!\n Общий балл: 110/110\n Вёрстка валидная +10\n Вёрстка семантическая +20\n Вёрстка соответствует макету +48\n Требования к css +12\n Интерактивность, реализуемая через css +20\n') 
+console.log("Привет!\n Если у тебя монитор 1440px и меньше, уменьши, пожалуйста, масштаб при проверке :)\n Хорошего дня тебе!\n Общий балл: 110/110\n Вёрстка валидная +10\n Вёрстка семантическая +20\n Вёрстка соответствует макету +48\n Требования к css +12\n Интерактивность, реализуемая через css +20\n");
+
+const burger = document.querySelector(".main-nav-burger"),
+  nav = document.querySelector(".main-nav"),
+  navLinks = document.querySelectorAll(".main-nav__link");
 
 const navSlide = () => {
-  const burger = document.querySelector('.main-nav-burger');
-  const nav = document.querySelector('.main-nav');
-
-  burger.addEventListener('click', () => {    
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('cross_maker');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("cross_maker");
   });
-}; 
+};
 
-navSlide()
+navSlide();
+
+navLinks.forEach((element) => element.addEventListener("click", closeMenu));
+nav.addEventListener('click', closeMenu);
+
+const closeMenu = (event) => {
+  if (event.target.classList.contains("main-nav__link")) {
+    nav.classList.remove("nav-active");
+    console.log('click');
+  }
+};
+
+closeMenu();
