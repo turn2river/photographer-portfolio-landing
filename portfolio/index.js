53a74f9d630bece11daa37052c2ee39ff56c825e
alexey.krylov <turn2river@gmail.com>
2022-01-27 15:13:10 +0300

    feat: implement function to change images, based on button click, add function to change active button class

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,32 +1,13 @@
-console.log(` TOTAL: 85 / 85 \n
-1. Вёрстка соответствует макету. Ширина экрана 768px +48 \n
-[x] блок <header> +6 \n
-[x] секция hero +6 \n
-[x] секция skills +6 \n 
-[x] секция portfolio +6 \n
-[x] секция video +6 \n
-[x] секция price +6 \n
-[x] секция contacts +6 \n
-[x] блок <footer> +6  \n
-2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15 \n
-[x] нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5 \n
-[x] нет полосы прокрутки при ширине страницы от 768рх до 480рх +5 \n
-[x] нет полосы прокрутки при ширине страницы от 480рх до 320рх +5 \n
-3. На ширине экрана 768рх и меньше реализовано адаптивное меню +22 \n
-[x] при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2 \n   
-[x] при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4 \n
-[x] высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4 \n
-[x] при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4 \n
-[x] бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2 \n
-[x] ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2 \n
-[x] при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4 `);
+console.log(` TOTAL: 85 / 85 \n `);
+
+/* BURGER MENU */
 
 const burger = document.querySelector('.main-nav-burger'),
   nav = document.querySelector('.main-nav'),
   navLinks = document.querySelectorAll('.main-nav__link'),
   logoLink = document.querySelector('.logo__icon');
 
-const burgerToggle = () => {
+const toggleBurgerClass = () => {
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burger.classList.toggle('burger-x');
@@ -35,20 +16,43 @@ const burgerToggle = () => {
 
 navLinks.forEach((element) =>
   element.addEventListener('click', () => {
-    burger.classList.remove('burger-x');
-    nav.classList.remove('nav-active');
+    removeMenuClasses();
   })
 );
 
-const logoBurgerRemover = () => {
+const logoClickListener = () => {
   logoLink.addEventListener('click', () => {
-    burger.classList.remove('burger-x');
-    nav.classList.remove('nav-active');
+    removeMenuClasses();
   });
 };
 
-const portfolioBtn = document.querySelector('.portfolio__button'),
+function removeMenuClasses() {
+  burger.classList.remove('burger-x');
+  nav.classList.remove('nav-active');
+}
+
+toggleBurgerClass();
+logoClickListener();
+
+/* IMAGE TABS */
+
+const portfolioBtns = document.querySelectorAll('.portfolio__button'),
+  portfolioBtnsParent = document.querySelector('.portfolio__buttons'),
   portfolioImages = document.querySelectorAll('.portfolio__image');
 
-burgerToggle();
-logoBurgerRemover();
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+portfolioBtnsParent.addEventListener('click', (event) => {
+  const target = event.target;
+  portfolioBtns.forEach((element) => {
+    element.classList.remove('portfolio__button-active');
+  });
+  if (target && target.classList.contains('portfolio__button')) {
+    portfolioImages.forEach((img, index) => {
+      img.src = `./assets/img/${target.dataset.season}/${index + 1}.jpg`;
+    });
+    target.classList.add('portfolio__button-active');
+  }
+});
+
+
